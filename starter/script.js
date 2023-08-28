92b6f09679da8000b7b17c2551b41b83b541a0d4
Mohamed Shehab <140072066+Shebo280@users.noreply.github.com>
2023-08-28 18:22:39 +0300

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -3,11 +3,13 @@
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
+// api for currency change
+const api = 'https://api.exchangerate-api.com/v4/latest/USD';
 
 // Data
 const account1 = {
   owner: 'Mohamed Shehab',
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  movements: [200, 455.23, -306.5, 2500, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 
@@ -110,22 +112,26 @@ const labelBalance = document.querySelector('.balance__value');
 const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelSumDebts = document.querySelector('.summary__value--debt');
 const labelTimer = document.querySelector('.timer');
 
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
 
 const btnLogin = document.querySelector('.login__btn');
+const btnLogout = document.querySelector('.logout__btn');
 const btnTransfer = document.querySelector('.form__btn--transfer');
 const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
 
+const loginForm = document.querySelector('.login');
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
 const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputLoanRequest = document.querySelector('.form__input--loan-request');
+const inputLoanPay = document.querySelector('.form__input--loan-pay');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 const options = {
@@ -135,13 +141,17 @@ const options = {
   month: 'numeric',
   year: 'numeric',
 };
-const formattedNumber = function (number) {
-  return new Intl.NumberFormat(currentAccount.locale, {
+
+const formattedNumber = function (number, acc = currentAccount) {
+  return new Intl.NumberFormat(acc.locale, {
     style: 'currency',
-    currency: currentAccount.currency,
+    currency: acc.currency,
     useGrouping: false,
   }).format(number);
 };
+//before starting
+btnLogout.disabled = true;
+
 const gettingDateFormat = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
@@ -167,15 +177,37 @@ const displayMovments = function (accs, sortCheck = false) {
       new Date(accs.movementsDates[index]),
       accs.locale
     );
-
-    const check = element < 0 ? 'withdrawal' : 'deposit';
+    //checking if we passed an array or not if the array then we check if it's loan or transfer then we check if the loan is request or pay , and the transfer from which account to which account
+    let check,
+      amount = element,
+      type;
+    if (typeof element === 'object') {
+      if (element.length == 2) {
+        if (element[0] > 0) {
+          type = 'Loan Requsted';
+          check = 'loanRequested';
+        } else {
+          type = 'Debt Payment';
+          check = 'loanPaid';
+        }
+      } else {
+        if (element[0] > 0) {
+          type = `transferd from ${element[2]}`;
+        } else type = `transferd to ${element[2]}`;
+        check = 'transfer';
+      }
+      amount = element[0];
+    } else {
+      check = element < 0 ? 'withdrawal' : 'deposit';
+      type = check;
+    }
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${check}">
-            ${index + 1} ${element < 0 ? 'withdrawal' : 'deposit'}
+            ${index + 1} ${type} 
     </div>
     <div class="movements__date">${date}</div>
-    <div class="movements__value">${formattedNumber(element)}</div>
+    <div class="movements__value">${formattedNumber(amount)}</div>
     </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -191,21 +223,27 @@ const userNameGenerator = function (accs) {
 };
 userNameGenerator(accounts);
 const displayBalance = function (accs) {
-  const balance = accs.movements.reduce((acc, cur) => (acc += cur), 0);
+  const balance = accs.movements
+    .flat()
+    .filter(item => typeof item === 'number')
+    .reduce((acc, cur) => (acc += cur), 0);
   accs.balance = balance;
   labelBalance.textContent = formattedNumber(balance);
 };
 const descriptionOfBalance = function (accounts) {
   let incomes = accounts.movements
+    .flat()
     .filter(item => item > 0)
     .reduce((acc, cur) => acc + cur, 0);
   let outcomes = Math.abs(
     accounts.movements
+      .flat()
       .filter(item => item < 0)
       .reduce((acc, cur) => acc + cur, 0)
   );
 
   let balance = accounts.movements
+    .flat()
     .filter(item => item > 0)
     .map(cur => (cur *= accounts.interestRate / 100))
     .filter(cur => cur > 1)
@@ -213,7 +251,6 @@ const descriptionOfBalance = function (accounts) {
   outcomes = +outcomes.toFixed(2);
   incomes = +incomes.toFixed(2);
   balance = +balance.toFixed(2);
-
   labelSumIn.textContent = formattedNumber(incomes);
   labelSumOut.textContent = formattedNumber(outcomes);
   labelSumInterest.textContent = formattedNumber(balance);
@@ -234,6 +271,7 @@ const timerLogout = function () {
       containerApp.style.opacity = 0;
       labelWelcome.textContent = 'Log in to get started';
       alert('You have been logged out');
+      showingLogin();
     }
     timer--;
     time -= 1000;
@@ -242,13 +280,54 @@ const timerLogout = function () {
   const timerFunction = setInterval(tick, 1000);
   return timerFunction;
 };
-let currentAccount, receiver, timerFunction;
+const hidingLogin = function () {
+  btnLogin.style.opacity = 0;
+  inputLoginPin.style.opacity = 0;
+  inputLoginUsername.style.opacity = 0;
+  btnLogin.disabled = true;
+  btnLogout.disabled = false;
+  btnLogout.style.opacity = 1;
+};
+const showingLogin = function () {
+  btnLogin.style.opacity = 1;
+  inputLoginPin.style.opacity = 1;
+  inputLoginUsername.style.opacity = 1;
+  btnLogin.disabled = false;
+  btnLogout.disabled = true;
+  btnLogout.style.opacity = 0;
+};
+const updateUI = function (acc) {
+  descriptionOfBalance(acc);
+  displayBalance(acc);
+  displayMovments(acc);
+};
+let currentAccount,
+  receiver,
+  timerFunction,
+  currentDebts = 0,
+  amounOfTransaction,
+  receiveTransaction = 0;
+function getResults() {
+  fetch(`${api}`)
+    .then(currency => {
+      return currency.json();
+    })
+    .then(displayResults);
+}
+function displayResults(currency) {
+  let fromRate = currency.rates[currentAccount.currency];
+  let toRate = currency.rates[receiver.currency];
+  receiveTransaction = (toRate / fromRate) * amounOfTransaction;
+}
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
+  if (currentAccount) labelSumDebts.textContent = formattedNumber(0);
   if (currentAccount?.pin === +inputLoginPin.value ? true : false) {
+    //hiding login
+    hidingLogin();
     //showing the page
     containerApp.style.opacity = 1;
     //clearing the input fields
@@ -257,7 +336,7 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginPin.blur();
     inputCloseUsername.blur();
     // welcoming the user
-    labelWelcome.textContent = `Welcome back ${currentAccount.owner}`;
+    labelWelcome.textContent = `Welcome back ${currentAccount.owner}.`;
     //showing the current date and the time.
     labelDate.textContent = new Intl.DateTimeFormat(
       currentAccount.locale,
@@ -269,21 +348,23 @@ btnLogin.addEventListener('click', function (e) {
     displayMovments(currentAccount);
   } else {
     currentAccount?.pin
-      ? alert('The password is wrong')
-      : alert('The username is invalid');
+      ? alert('The password is wrong.')
+      : alert('The username is invalid.');
   }
   // checking if there is a timer running then stop it
   if (timerFunction) clearInterval(timerFunction);
   timerFunction = timerLogout();
 });
-const updateUI = function (acc) {
-  descriptionOfBalance(acc);
-  displayBalance(acc);
-  displayMovments(acc);
-};
+btnLogout.addEventListener('click', function (e) {
+  e.preventDefault();
+  showingLogin();
+  containerApp.style.opacity = 0;
+  labelWelcome.textContent = 'Log in to get started.';
+  alert('Your have been logged out.');
+});
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amounOfTransaction = +inputTransferAmount.value;
+  amounOfTransaction = +inputTransferAmount.value;
   receiver = accounts.find(acc => acc.username === inputTransferTo.value);
   if (
     receiver &&
@@ -291,24 +372,40 @@ btnTransfer.addEventListener('click', function (e) {
     amounOfTransaction > 0 &&
     receiver?.username !== currentAccount.username
   ) {
-    currentAccount.balance -= amounOfTransaction;
-    labelBalance.textContent = `${currentAccount.balance}$`;
-    currentAccount.movements.push(-amounOfTransaction);
-    receiver.movements.push(amounOfTransaction);
-    //pushing the date
-    currentAccount.movementsDates.push(new Date().toISOString());
-    receiver.movementsDates.push(new Date().toISOString());
-    updateUI(currentAccount);
-    //clearing the input fields
+    //clearing the input fields\
+    getResults();
     inputTransferAmount.value = inputTransferTo.value = '';
     //removing the cursor from the field
     inputTransferAmount.blur();
     inputTransferTo.blur();
+    setTimeout(() => {
+      currentAccount.balance -= amounOfTransaction;
+      labelBalance.textContent = `${currentAccount.balance}$`;
+      currentAccount.movements.push([
+        -amounOfTransaction,
+        'transfer',
+        receiver.owner,
+      ]);
+      receiver.movements.push([
+        receiveTransaction,
+        'transfer',
+        currentAccount.owner,
+      ]);
+      alert(
+        `You have sent ${formattedNumber(amounOfTransaction)} to ${
+          receiver.owner
+        } which equals ${formattedNumber(receiveTransaction, receiver)}.`
+      );
+      //pushing the date
+      currentAccount.movementsDates.push(new Date().toISOString());
+      receiver.movementsDates.push(new Date().toISOString());
+      updateUI(currentAccount);
+    }, 2000);
   } else {
-    if (!receiver) alert('There is no account got such name');
+    if (!receiver) alert('There is no account got such name.');
     else if (amounOfTransaction > currentAccount.balance)
-      alert("You don't have enough money");
-    else if (amounOfTransaction < 0) alert("You can't enter negative +");
+      alert("You don't have enough money.");
+    else if (amounOfTransaction < 0) alert("You can't enter negative number.");
     else alert("You can't transfer money to yourself!");
   }
   //reset the timer if he made a transaction
@@ -331,8 +428,9 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.blur();
     //Hiding UI
     containerApp.style.opacity = 0;
-    labelWelcome.textContent = 'Log in to get started';
-    alert('Your account has been deleted');
+    labelWelcome.textContent = 'Log in to get started.';
+    alert('Your account has been deleted.');
+    showingLogin();
   } else if (inputCloseUsername.value != currentAccount.username)
     alert('Wrong username entered!');
   else alert('Wrong password entered!');
@@ -341,25 +439,61 @@ btnClose.addEventListener('click', function (e) {
 });
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const requestedloan = Math.floor(+inputLoanAmount.value);
+  const requestedloan = Math.floor(+inputLoanRequest.value);
+  const paidloan = Math.floor(+inputLoanPay.value);
   //if there is any deposite is more than 10% of requested loan then accept it
-  alert('It will take 5 seconds to answer your request');
-  setTimeout(() => {
-    if (currentAccount.movements.some(item => item >= requestedloan / 10)) {
-      //takes 3 seconds to answer
-
-      alert('Your loan has been accepted');
-      currentAccount.movements.push(requestedloan);
+  if (paidloan && requestedloan) {
+    alert(`You can't do two transactions at the same time.`);
+    inputLoanRequest.value = inputLoanPay.value = '';
+    inputLoanRequest.blur();
+    inputLoanPay.blur();
+  } else {
+    alert('It will take 3 seconds to answer your request.');
+    inputLoanRequest.value = inputLoanPay.value = '';
+    inputLoanRequest.blur();
+    inputLoanPay.blur();
+    setTimeout(() => {
+      if (requestedloan) {
+        if (currentAccount.movements.some(item => item >= requestedloan / 10)) {
+          //takes 3 seconds to answer
+          alert('Your loan has been accepted.');
+          currentAccount.movements.push([requestedloan, 'loanRequested']);
+          currentDebts += requestedloan;
+        } else alert('Unfortunately, Your loan has been refused.');
+      } else {
+        if (currentAccount.balance > paidloan) {
+          if (!currentDebts) {
+            alert("You don't have any depts.");
+          } else {
+            alert(
+              `You have paid ${formattedNumber(
+                currentDebts > paidloan ? paidloan : currentDebts
+              )} from your depts, ${
+                currentDebts - paidloan <= 0
+                  ? `you don't have depts anymore `
+                  : `The remaning are: `
+              }${formattedNumber(currentDebts - paidloan)}.`
+            );
+            if (paidloan > currentDebts) {
+              currentAccount.movements.push([-currentDebts, 'loanPaid']);
+              currentDebts = 0;
+            } else {
+              currentAccount.movements.push([-paidloan, 'loanPaid']);
+              currentDebts -= paidloan;
+            }
+          }
+        } else alert(`You don't have enough money.`);
+      }
       // adding current date
       currentAccount.movementsDates.push(new Date().toISOString());
       updateUI(currentAccount);
-    } else alert('Unfortunately, Your loan has been refused');
-    inputLoanAmount.value = '';
-    inputLoanAmount.blur();
-    //reset the timer if he requested a loan
-    if (timerFunction) clearInterval(timerFunction);
-    timerFunction = timerLogout();
-  }, 5000);
+      // showing the current debts
+      labelSumDebts.textContent = formattedNumber(currentDebts);
+      //reset the timer if he requested a loan
+      if (timerFunction) clearInterval(timerFunction);
+      timerFunction = timerLogout();
+    }, 3000);
+  }
 });
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
@@ -369,5 +503,3 @@ btnSort.addEventListener('click', function (e) {
   // if it's true then we change it to false and otherwise
   sorted = !sorted;
 });
-
-
